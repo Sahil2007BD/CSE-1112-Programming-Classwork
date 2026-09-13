04864644f2ec3b22e27c0e8b43a1d8d804ccd6c6
Sahil <sahiledu2007@gmail.com>
2026-09-13 19:34:07 +0600

    Added File

---
+++ 13092026_Lab/Sahil.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    char n[20];
+    char d[20];
+    char u[50];
+    printf("What is your name?: ");
+    scanf("%s", n);
+    printf("What is your department?: ");
+    scanf("%s", d);
+    printf("What is your university?: ");
+    scanf("%s", u);
+
+    printf("My name is %s\n", n);
+    printf("Dept. of %s\n", d);
+    printf("%s\n", u);
+
+    return 0;
+}
